9f527f92a07c0bf1cf2131f9a4b61201c4470916
Dipiri-Lap <74485224+Dipiri-Lap@users.noreply.github.com>
2026-03-14 01:36:46 +0900

    feat: Initialize React application entry point with StrictMode and BrowserRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+console.log('[PuzzleGarden] build: 2026-03-14-v2');
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
